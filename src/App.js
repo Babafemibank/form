4fc70933144be61b794af9c22aa8710ac84a9887
Babafemi Usman <babafemiusman@gmail.com>
2024-01-05 00:47:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
+  const[submitted, setSubmitted] = useState(false)
+
+  useEffect(() =>{
+    if(firstName && lastName &&validateEmail(email))
+    {
+setIsFormValid(true)
+    }
+    else{
+      setIsFormValid(false)
+    }
+  },[firstName,lastName,email])
+  const validateEmail = (email) => {
+    // Regular expression to validate email address
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+   
+      setFirstNameError(e.target.value? '' :'first name is required' );
+   
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+    setLastNameError(e.target.value? '' : 'last name is required');
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value === '') {
+      setEmailError('Email is required');
+    } else  {
+      setEmailError(validateEmail(e.target.value)?  "" :'please enter a valid email address');
+    } 
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && lastName && validateEmail(email)) {
+      
+    } else {
+      setFirstNameError(firstName ? '' : 'first name is required');
+      setLastNameError(lastName ? '' : 'last name is required');
+      setEmailError(email ? 'please enter a valid email address' : 'Email is required');
+      setIsFormValid(false);
+    }
+    setSubmitted(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+     {submitted? <div className="success">Success! Thanks for registering.</div> : null}
+     <div> <input type="text" value={firstName} onChange={handleFirstNameChange} placeholder="first name"/>
+        {firstNameError && <span>{firstNameError}</span>} </div>
+        <div>
+        <input type="text" value={lastName} onChange={handleLastNameChange} placeholder="last name"/>
+        {lastNameError && <span>{lastNameError}</span>}</div>
+        <div>
+        <input type="email" value={email} onChange={handleEmailChange} placeholder="email address"/>
+        {emailError && <span>{emailError}</span>}
+        </div>
+        <div>
+      <button type="submit" disabled={!isFormValid}>Submit</button></div>
+    </form>
   );
 }
 
